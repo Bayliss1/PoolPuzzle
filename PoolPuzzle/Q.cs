f44f8b12c24d32b28b16bf158e4af6ed9b62e841
Owen <owen.bayliss@sky.com>
2022-09-06 19:55:55 +0100

    Add project files.

---
+++ PoolPuzzle/Q.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPuzzle
+{
+    class Q
+    {
+        public Q(bool add)
+        {
+            if (add) Op = "+";
+            else Op = "*";
+            N1 = 
+            N2 = 
+        }
+
+        public static Random R = new Random();
+        public int N1 { get; private set; }
+        public string Op { get; private set; }
+        public int N2 { get; private set; }
+
+        public int Check(int a)
+        {
+            if (Op == "+") return (a = N1 + N2);
+            else return (a = N1 * N2);
+        }
+    }
+}
